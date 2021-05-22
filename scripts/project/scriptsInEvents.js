6bd8b2ca7f12a5ce7a8d50c31d369a6eb6fb4077
alinadidenk <79787045+alinadidenk@users.noreply.github.com>
2021-05-22 15:42:36 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async списоксобытийуровня1_Event3_Act3(runtime, localVars)
+		{
+			спрайт6
+			
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
